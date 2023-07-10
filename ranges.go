b19aa5c7210590e0eef32be71481dd6fab9b48f4
1Shubham7 <shubhammahar1306@gmail.com>
2023-07-10 12:28:38 +0530

    ranges

--- ranges.go
+++ ranges.go
@@ -5,7 +5,18 @@ import (
 )
 
 func main() {
-	arr := []int{1,2,3}
+	var n int
+	fmt.Print("How many number you want to enter")
+	fmt.Scan(&n)
+	fmt.Print("Enter the numbers")
+	arr := make([]int,n)
+	for i:=0; i<n; i++ {
+		fmt.Scan(&arr[i])
+	}
+	
 	sum := 0
-	for _,num := range nums
+	for _,num := range arr {
+		sum += num
+	}
+	fmt.Print(sum)
 }
