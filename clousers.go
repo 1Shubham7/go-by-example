0fec517eb18d2965bdb48f5fb7f47e07f33dcb57
1Shubham7 <shubhammahar1306@gmail.com>
2023-07-10 17:18:43 +0530

    explained clousers

--- clousers.go
+++ clousers.go
@@ -14,6 +14,10 @@ func main() {
 	fmt.Println(a())
 }
 
+// closures function : here the intSeq function returns another function
+// that function returns i, therefore whenever you call intSeq, another function 
+// is created. Look at the code carefull, you will get it!
+
 func intSeq() func() int{
 	i := 0
 	return func() int{
