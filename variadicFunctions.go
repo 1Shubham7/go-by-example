39c1f1dd18fbdfc4f325c9ba802d1f5ddc4858ed
1Shubham7 <shubhammahar1306@gmail.com>
2023-07-10 16:40:46 +0530

    program 8 -> variadic function

---
+++ variadicFunctions.go
@@ -0,0 +1,17 @@
+package main
+
+import (
+	"fmt"
+)
+
+func main() {
+	fmt.Print(sume(1,2,3,4,5))
+}
+
+func sume(nums ... int) int{
+	total := 0
+	for _, num := range nums{
+		total += num
+	}
+	return total
+}
